77cb3626a8f66d01df9d00c3d92c9f0b0a4480de
ssccabi <lehellel@gmail.com>
2023-02-23 00:23:10 +0100

    onemore

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from "ui";
-import { Page } from "../../compos/src/stories/Page"
+import { Page } from "../../compos/src/stories/components/Page"
 
 export default function Docs() {
   return (
